1eae2129ee737f4afaffeb59d2d385bd082b3a1d
Acchu <acchu1915@gmail.com>
2024-01-31 06:45:42 +0530

    Partial Completion and BugFix
    Partial completion of checkGameBoardWinner
    Fixed issue where player could overwrite arrayvalue

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@ function CreatePlayer(playerName, value){
     this.value = value;
 }
 
+CreatePlayer.prototype.bIsWinnerPresent = false;
+CreatePlayer.prototype.bIsPlayerTurn = true;
+
 const GameBoard = (function (){
     const gameBoard = [];
     while(gameBoard.length < 3){
@@ -10,23 +13,46 @@ const GameBoard = (function (){
         gameBoard.push(row);
     }
     
-    function getGameBoard(indexFirst,indexSecond){
+    function getGameBoardIndex(indexFirst,indexSecond){
         return gameBoard[indexFirst][indexSecond];
     }
 
     function setValueGameBoard(indexFirst,indexSecond,value){
-        // Put a condition to check if value in place of mentioned indexes or else i will be able to overwrite values
         gameBoard[indexFirst][indexSecond] = value;
         displayGameBoard();
     }
 
     function displayGameBoard(){
-        console.log([...gameBoard]);
-        console.log(gameBoard[0][1])
+        console.log(
+            JSON.parse(
+              JSON.stringify(gameBoard)
+            )
+          ); // Code provided by tobyplaysuke can be useful for debugging and also solves the issue which occurs when a normal console.log is done with array
+        // console.log(gameBoard);
     }
     
+    function checkGameBoardWinner(){
+        // 4 Comparisons when center is chosen - Could put all with a || but will be harder to troubleshoot maybe loop it?
+        if(gameBoard[1][1] === gameBoard[0][0] && gameBoard[1][1] === gameBoard[2][2]){
+            return gameBoard[1][1];
+        }
+        if(gameBoard[1][1] === gameBoard[2][0] && gameBoard[1][1] === gameBoard[0][2]){
+
+        }
+        if(gameBoard[1][1] === gameBoard[0][1] && gameBoard[1][1] === gameBoard[2][1]){
+
+        }
+        if(gameBoard[1][1] === gameBoard[1][0] && gameBoard[1][1] === gameBoard[1][2]){
+
+        }
+        // 2 Comparisons when bottom right is chosen
+        if()
+        // 2 Comparisons when top left is chosen 
+        
+    }
+
     return {
-        getGameBoard,
+        getGameBoardIndex,
         setValueGameBoard,
         displayGameBoard
     }
@@ -41,7 +67,29 @@ function playGame(){
     const player1 = new CreatePlayer("Player1","X") //prompt("Enter Name","test")
     const player2 = new CreatePlayer("Player2", "O");    
     GameBoard.displayGameBoard();
-    GameBoard.setValueGameBoard(0,1,player1.value)
+    // !CreatePlayer.prototype.bIsWinnerPresent
+    let i = 0
+    while(i < 5){
+        let indexOneValue = parseInt(prompt("Enter Value Location"));
+        let indexTwoValue = parseInt(prompt("Enter Value Location"));
+        while(GameBoard.getGameBoardIndex(indexOneValue,indexTwoValue) === 'X' || GameBoard.getGameBoardIndex(indexOneValue,indexTwoValue) === 'O'){
+            indexOneValue = parseInt(prompt("Location Taken Enter A New Value First Index Location"));
+            indexTwoValue = parseInt(prompt("Location Taken Enter A New Value Second Index Location"));
+        }
+        if(i > 4){
+
+        }
+        if(CreatePlayer.prototype.bIsPlayerTurn){
+            GameBoard.setValueGameBoard(indexOneValue,indexTwoValue,player1.value);
+            CreatePlayer.prototype.bIsPlayerTurn = false;
+        }
+        else{
+            GameBoard.setValueGameBoard(indexOneValue,indexTwoValue,player2.value);
+            CreatePlayer.prototype.bIsPlayerTurn = true;
+        }
+        // CreatePlayer.prototype.bIsWinnerPresent=true;
+        i++;
+    }
 }
 
 newGame();
